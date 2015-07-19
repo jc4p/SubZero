cbd0e5d47f37ab4af8a98b169871038f47f97ff3
Kasra Rahjerdi <contact@kasrarahjerdi.com>
2015-07-19 01:40:20 -0400

    Add empty secrets file

---
+++ secrets.py
@@ -0,0 +1,7 @@
+import os
+
+SNS_TOPIC 		= os.environ.get('SNS_TOPIC', '')
+SNS_APPLICATION	= os.environ.get('SNS_APPLICATION', '')
+
+AWS_ACCESS_KEY	= os.environ.get('AWS_ACCESS_KEY')
+AWS_SECRET_KEY	= os.environ.get('AWS_SECRET_KEY')
